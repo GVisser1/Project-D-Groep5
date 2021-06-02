1b92bd04dc14c00a17c9c38376313eebafee9a76
GVisser1 <55433031+GVisser1@users.noreply.github.com>
2021-06-02 11:37:18 +0200

    Renoved redundant file + usings

--- Bitfit/Bitfit/Models/Workout.cs
+++ Bitfit/Bitfit/Models/Workout.cs
@@ -1,9 +1,5 @@
-﻿using Bitfit.Pages;
-using System;
-using System.Collections.Generic;
+﻿using System;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace Bitfit.Models
 {
--- Bitfit/Bitfit/Pages/UserPage.razor.cs
+++ Bitfit/Bitfit/Pages/UserPage.razor.cs
@@ -1,11 +1,6 @@
-﻿using Microsoft.AspNetCore.Components;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Bitfit.Models;
 using Microsoft.AspNetCore.Components.Forms;
-using System.Data;
 
 namespace Bitfit.Pages
 {
